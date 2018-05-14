31666d4eb6037c72093ebd747cd3dec1e6a2932a
Jide Olayinka <olajamesjide@gmail.com>
2018-05-14 08:14:17 +0100

    Implementing management

---
+++ IceLux/IceLux/Services/IStakingManagementService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceLux.Services
+{
+    public interface IStakingManagementService
+    {
+        /*IEnumerable<ContactType> GetContactTypes(ICriteria criteria);
+
+        IEnumerable<ContactType> GetAllContactTypes();
+
+        int GetTotalContactTypes();
+
+        ContactType CreateContactType(ContactType contactType);
+
+        ContactType GetContactType(int id);
+
+        ContactType UpdateContactType(ContactType contactType);
+
+        ContactType DeleteContactType(ContactType contactType);
+
+
+        IEnumerable<ContactInfo> GetContacts(ICriteria criteria);
+
+        int GetTotalContacts();
+
+        Contact CreateContact(Contact contact);
+
+        Contact GetContact(int id);
+
+        Contact UpdateContact(Contact contact);
+
+        Contact DeleteContact(Contact contact);*/
+    }
+}
